5a378bd37983109583847632a820fbf6afa36ac8
Bhanu Singh <122738012+bhanusingh008@users.noreply.github.com>
2023-06-08 13:26:20 +0530

    Update Main.java
    Created two classes and same method names, to implement overriding of method, From a child class.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
 public class Main {
-  
+      public static void main(String[] args) {
+
+        A a = new A();
+
+        B b = new B();
+
+        System.out.println(a.meth());
+        System.out.println(b.meth());
+
+    }
+
+    static class A{
+
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    static class B extends A{
+
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+
+    }
 }
